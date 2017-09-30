8f0a2275f5a8fc3b305590039866d2c11daf8d9b
macctown <macctown@hotmail.com>
2017-09-30 09:07:42 -0400

    Init Commit - static version
    static version with simple search

---
+++ search.js
@@ -0,0 +1,73 @@
+/* global instantsearch */
+
+app({
+  appId: '0IOFMKDDF9',
+  apiKey: '1199a140b2bf6b17dec82215291f23de',
+  indexName: 'hr_email'
+});
+
+function app(opts) {
+  // ---------------------
+  //
+  //  Init
+  //
+  // ---------------------
+  const search = instantsearch({
+    appId: opts.appId,
+    apiKey: opts.apiKey,
+    indexName: opts.indexName,
+    urlSync: true,
+  });
+
+  // ---------------------
+  //
+  //  Default widgets
+  //
+  // ---------------------
+  search.addWidget(
+    instantsearch.widgets.searchBox({
+      container: '#search-input',
+      placeholder: 'Search for recruiter by company, position, location, tech...',
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.hits({
+      container: '#hits',
+      hitsPerPage: 10,
+      templates: {
+        item: getTemplate('hit'),
+        empty: getTemplate('no-results'),
+      },
+      transformData: {
+        item: function (item) {
+          return item;
+        },
+      },
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.pagination({
+      container: '#pagination',
+      scrollTo: '#search-input',
+    })
+  );
+
+  search.start();
+}
+
+// ---------------------
+//
+//  Helper functions
+//
+// ---------------------
+function getTemplate(templateName) {
+  return document.querySelector(`#${templateName}-template`).innerHTML;
+}
+
+function getHeader(title) {
+  return `<h5>${title}</h5>`;
+}
+
+
